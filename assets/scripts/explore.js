4a34c2c5979d883b9c69e6897a65328f10fa4bca
MichinoriW <mwada@ucsd.edu>
2023-05-07 23:09:12 -0700

    part 2... i tried

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,17 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const synth = window.speechSynthesis;
+  let voices = [];
+  const inputForm = document.querySelector("#text-to-speech");
+  const voiceSelect = document.querySelector("select");
+  window.speechSynthesis.onvoiceschanged;
+  voices = synth.getVoices();
+  console.log(voices.length);//why length 0???
+  for(let i = 0; i < voices.length; i++){
+    let option = document.createElement('option');
+    console.log(option);
+    option.textContent = voices[i];
+    document.querySelector('#voice-select').appendChild(option);
+  }
 }
